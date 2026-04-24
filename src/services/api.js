bbe3fa6516ddd3d8de52086d7c5d33eb2a3ce648
algovista2026 <algovista2025@gmail.com>
2026-04-24 18:56:44 +0530

    Configure dynamic API URL for local vs production environments

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+const API_BASE = isLocalhost 
+  ? 'http://localhost:5000/api' 
+  : 'https://flexibond-backend-vkc0.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE,
